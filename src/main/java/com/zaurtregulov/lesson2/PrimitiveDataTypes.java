8c0300d3b8bfce95145b83724b2cb0353df2d23e
SergeyB19 <my.h.m@yandex.ru>
2023-12-27 12:41:49 +0300

    PrimitiveDataTypes

---
+++ src/main/java/com/zaurtregulov/lesson2/PrimitiveDataTypes.java
@@ -0,0 +1,71 @@
+package com.zaurtregulov.lesson2;
+
+public class PrimitiveDataTypes {
+    public static void main(String[] args) {
+        byte b1 = 10;
+        byte b2 = 20;
+        byte b3 = 100;
+
+        short s1 = 5;
+        short s2 = -10;
+        short s3 = 0;
+
+        int i1 = 500;
+
+        long l1 = 1000;
+        long l2 = 10000000000L;
+
+        float f1 = 3.14F;
+        float f2 = 2.5f;
+        float f3 = 20;
+
+        double d1 = 5.5;
+        double d2 = 87.65;
+
+
+        char c1 = 'a';
+        char c2 = 'A';
+        char c3 = '7';
+        char c4 = 'Ъ';
+        char c5 = 300;
+        char c6 = 500;
+        char c7 = '\u1234';
+
+        boolean bool1 = true;
+        boolean bool2 = false;
+
+        int a1 = 60;
+        int a2 = 0B111100;
+        int a3 = 0b111100;
+        int a4 = 074;
+        int a5 = 0x3c;
+        int a6 = 0X3C;
+
+        int a7 = 1_000_000_0______________0_0;
+
+        System.out.println(a7);
+        System.out.println("--------------");
+        System.out.println(a1);
+        System.out.println(a2);
+        System.out.println(a3);
+        System.out.println(a4);
+        System.out.println(a5);
+        System.out.println(a6);
+        System.out.println("Privet!!!");
+        System.out.println(b1);
+        System.out.println(s2);
+        System.out.println(i1);
+        System.out.println(l2);
+        System.out.println(f1);
+        System.out.println(d2);
+        System.out.println(c2);
+        System.out.println(c4);
+        System.out.println(c5);
+        System.out.println(c6);
+        System.out.println(c7);
+        System.out.println(bool1);
+        System.out.println(bool2);
+
+
+    }
+}
