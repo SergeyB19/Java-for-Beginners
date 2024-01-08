34bc5bb7ef40d45c2f5d384c1cb6b2d1ab805fe1
SergeyB19 <my.h.m@yandex.ru>
2024-01-08 11:15:54 +0300

    import static

--- src/main/java/lesson10hw/p1/p2/B.java
+++ src/main/java/lesson10hw/p1/p2/B.java
@@ -3,6 +3,11 @@ package lesson10hw.p1.p2;
 public class B {
     public static int a = 10;
     public static int b = 30;
+    public static boolean b1 = true;
+
+    public void showBoolean() {
+        System.out.println(b1);
+    }
 
 
 }
--- src/main/java/lesson10hw/p4/D.java
+++ src/main/java/lesson10hw/p4/D.java
@@ -9,12 +9,12 @@ import lesson10hw.p4.p5.E.*;
 import static lesson10hw.p1.p2.B.*;
 
 public class D {
-    A a = new A();
-    C c = new C();
-    E e = new E();
-    B b = new B();
 
     public static void main(String[] args) {
+        A a = new A();
+        C c = new C();
+        E e = new E();
+        B b = new B();
         System.out.println(B.a);
         System.out.println(B.b);
         System.out.println(E.f);
