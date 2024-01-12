b09f1d8c7eb7041004a8768a26c51e6f7e119fd3
SergeyB19 <my.h.m@yandex.ru>
2024-01-12 15:26:46 +0300

    while

---
+++ src/main/java/lesson15/Test9.java
@@ -0,0 +1,22 @@
+package lesson15;
+
+public class Test9 {
+    public static void main(String[] args) {
+
+        int chas = -1;
+        OUTER:
+        do {
+            chas++;
+            int minuta = 0;
+            INNER:
+            while (minuta < 60) {
+                if (minuta == 20) {
+                    continue OUTER;
+                }
+                System.out.println(chas + ":" + minuta);
+                minuta++;
+            }
+        }
+        while (chas < 24);
+    }
+}
