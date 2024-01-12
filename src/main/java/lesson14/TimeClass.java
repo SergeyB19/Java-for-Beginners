68a8b016591364756713c865e3afc4e1a09e192a
SergeyB19 <my.h.m@yandex.ru>
2024-01-12 14:29:57 +0300

    regular for i loop

--- src/main/java/lesson14/TimeClass.java
+++ src/main/java/lesson14/TimeClass.java
@@ -6,18 +6,23 @@ public class TimeClass {
     }
 
     public static void timer() {
-        for (int hour = 0; hour <=6 ; hour++) {
+        OUTER:
+        for (int hour = 0; hour <= 6; hour++) {
+            MIDDLE:
             for (int minuta = 0; minuta < 60; minuta++) {
+                if (hour > 1 && minuta % 10 == 0) {
+                    break OUTER;
+                }
+                INNER:
                 for (int secunda = 0; secunda < 60; secunda++) {
-                    if (hour > 1 && minuta % 10 == 0) {
-                        break;
-                    }
                     if (secunda * hour > minuta) {
-                        continue;
+                    continue MIDDLE;
                     }
                     System.out.println(hour + ":" + minuta + ":" + secunda);
                 }
+
             }
         }
     }
 }
+
