9af29df228354553c4bf3e2585c914577d9dee90
SergeyB19 <my.h.m@yandex.ru>
2024-01-23 09:16:36 +0300

    encapsulation

--- src/main/java/lesson23/homework2/Test.java
+++ src/main/java/lesson23/homework2/Test.java
@@ -3,7 +3,7 @@ package lesson23.homework2;
 public class Test {
     public static void main(String[] args) {
         Dog dog = new Dog("Dog");
-        System.out.println(dog.paw);
+        System.out.println("Кол-во лап у собаки: " + dog.paw);
         Cat cat = new Cat("Cat");
         cat.sleep();
     }
